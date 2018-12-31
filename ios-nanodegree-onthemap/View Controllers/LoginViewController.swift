917dc52393fd144cdb4b36b3f7d6fce24d77a18e
Andrew Despres <21027320+a-despres@users.noreply.github.com>
2018-12-30 21:15:23 -0800

    Modify UI of LoginViewController
    This commit updates the UI of the LoginViewController by adding the Udacity Logo, adding a gradient background, and adding a sign up button for users without a Udacity account. Tapping on the Sign Up button opens the sign up form in Safari.

--- ios-nanodegree-onthemap/View Controllers/LoginViewController.swift
+++ ios-nanodegree-onthemap/View Controllers/LoginViewController.swift
@@ -35,8 +35,28 @@ class LoginViewController: UIViewController {
         }
     }
     
+    @IBAction func handleSignUpButtonTap(_ sender: UIButton) {
+        let url = URL(string: "https://auth.udacity.com/sign-up")!
+        UIApplication.shared.open(url)
+    }
+    
     // MARK: - View Controller
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // define gradient colors
+        let color1 = UIColor(red: 236/255, green: 240/255, blue: 241/255, alpha: 1)
+        let color2 = UIColor(red: 41/255, green: 128/255, blue: 185/255, alpha: 1)
+        
+        // add gradient to view background
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [color1.cgColor, color2.cgColor]
+        view.layer.insertSublayer(gradientLayer, at: 0)
+        
+        // modify view of loginButton
+        loginButton.layer.borderColor = UIColor.white.cgColor
+        loginButton.layer.borderWidth = 2
+        loginButton.layer.cornerRadius = 8
     }
 }
