b926d2ffd89dc9f1884b65fbe95d6695e52438b4
Andrew Despres <21027320+a-despres@users.noreply.github.com>
2019-01-01 19:23:15 -0800

    Refactor ConfirmLocationViewController and add Network Activity Indicator
    This commit refactors the code for ConfirmLocationViewController to make it a little more readable. Also included in this commit is the addition of an activity indicator that is displayed when there is an active network request. This provides much need visual feedback for the user after clicking on the share/finish button.

--- ios-nanodegree-onthemap/View Controllers/ConfirmLocationViewController.swift
+++ ios-nanodegree-onthemap/View Controllers/ConfirmLocationViewController.swift
@@ -26,14 +26,14 @@ class ConfirmLocationViewController: UIViewController {
     
     // MARK: - IBActions
     @IBAction func handleFinishButtonTap(_ sender: UIButton) {
-        guard let locationName = placemark?.name, let location = placemark?.location else {
-            return
-        }
+        // Make sure that location and user data exists, else fail
+        guard let locationName = placemark?.name, let location = placemark?.location else { return }
+        guard let userData = appDelegate.userData else { return }
         
-        guard let userData = appDelegate.userData else {
-            return
-        }
+        // Hide the iconView, enable the activity indicatorand disable finish button
+        prepareUIForNetworkRequest()
         
+        // Now that we know location and user data exists, lets make a StudentLocation object to share
         let studentLocation = StudentLocation(firstName: userData.firstName,
                                               lastName: userData.lastName,
                                               latitude: location.coordinate.latitude,
@@ -43,26 +43,13 @@ class ConfirmLocationViewController: UIViewController {
                                               objectId: appDelegate.studentLocation?.objectId ?? "",
                                               uniqueKey: userData.key)
         
+        // share the newly created StudentLocation object
         if appDelegate.studentLocation == nil {
-            // post new student location to database
-            OnTheMap.postStudentLocation(studentLocation) { [unowned self] (response, error) in
-                if let _ = response {
-                    self.presentingViewController?.dismiss(animated: true, completion: nil)
-                } else {
-                    if let error = error {
-                        self.present(ErrorAlert.Alert.postStudentLocation(error).alertController, animated: true, completion: nil)
-                    }
-                }
-            }
+            // post new StudentLocation to database
+            OnTheMap.postStudentLocation(studentLocation, completion: handlePostStudentLocationResponse(_:error:))
         } else {
-            // update(put) student location in database
-            OnTheMap.putStudentLocation(studentLocation) { [unowned self] (response, error) in
-                if let _ = response {
-                    self.presentingViewController?.dismiss(animated: true, completion: nil)
-                } else if let error = error {
-                    self.present(ErrorAlert.Alert.postStudentLocation(error).alertController, animated: true, completion: nil)
-                }
-            }
+            // update(put) StudentLocation in database
+            OnTheMap.putStudentLocation(studentLocation, completion: handlePutStudentLocationResponse(_:error:))
         }
     }
     
@@ -83,16 +70,7 @@ class ConfirmLocationViewController: UIViewController {
         finishButton.layer.cornerRadius = 8
         
         // place pin and center map
-        if let location = placemark?.location {
-            let pin = MKPointAnnotation()
-            pin.coordinate = location.coordinate
-
-            self.mapView.setCenter(pin.coordinate, animated: false)
-            self.mapView.setVisibleMapRect(self.mapView.visibleMapRect, edgePadding: UIEdgeInsets(top: -0.05, left: 0, bottom: 0, right: 0), animated: false)
-
-            self.mapView.camera.altitude = 4096
-            self.mapView.addAnnotation(pin)
-        }
+        if let location = placemark?.location { markLocation(location) }
     }
     
     override func viewDidLayoutSubviews() {
@@ -104,5 +82,104 @@ class ConfirmLocationViewController: UIViewController {
         gradientLayer.colors = [UIColor.black.cgColor, UIColor.black.cgColor, UIColor.clear.cgColor]
         gradientLayer.locations = [0, 0.3, 0.85, 1]
         mapView.layer.mask = gradientLayer
+    }
+}
+
+// MARK: - Error Handling
+extension ConfirmLocationViewController {
+    
+    /**
+     Display a UIAlertView for the given error.
+     - parameter error: The error to be parsed and displayed.
+     */
+    private func displayAlertForError(_ error: Error) {
+        if let error = error as? URLError {
+            present(ErrorAlert.forURLError(error), animated: true, completion: nil)
+        } else {
+            present(ErrorAlert.forUnknownError(error), animated: true, completion: nil)
+        }
+    }
+}
+
+// MARK: - Map View
+extension ConfirmLocationViewController {
+    
+    /**
+     Place a pin on the map and position in view.
+     - parameter location: The location to be marked with a pin.
+     */
+    private func markLocation(_ location: CLLocation) {
+        // Define pin
+        let pin = MKPointAnnotation()
+        pin.coordinate = location.coordinate
+        
+        // Define offset so that pin is not perfectly centered
+        let edgeInsets = UIEdgeInsets(top: -0.05, left: 0, bottom: 0, right: 0)
+        
+        // place pin and "center" in view
+        mapView.addAnnotation(pin)
+        mapView.setCenter(pin.coordinate, animated: false)
+        mapView.setVisibleMapRect(mapView.visibleMapRect, edgePadding: edgeInsets, animated: false)
+        mapView.camera.altitude = 4096
+    }
+}
+
+// MARK: - OnTheMap API Responses
+extension ConfirmLocationViewController {
+    
+    /**
+     Handle API Response for postStudentLocation.
+     - parameter response: The PostStudentLocation object returned by the API call. (Optional)
+     - parameter error: The error returned by the API call. (Optional)
+     */
+    private func handlePostStudentLocationResponse(_ response: PostStudentLocation?, error: Error?) {
+        // reset UI now that we're back from making a network request
+        resetUIAfterNetworkRequest()
+        
+        // handle error if necessary, otherwise handle the response
+        if let error = error {
+            displayAlertForError(error)
+        } else {
+            if let _ = response {
+                presentingViewController?.dismiss(animated: true, completion: nil)
+            }
+        }
+    }
+    
+    /**
+     Handle API Response for putStudentLocation.
+     - parameter response: The PutStudentLocation object returned by the API call. (Optional)
+     - parameter error: The error returned by the API call. (Optional)
+     */
+    private func handlePutStudentLocationResponse(_ response: PutStudentLocation?, error: Error?) {
+        // reset UI now that we're back from making a network request
+        resetUIAfterNetworkRequest()
+        
+        // handle error if necessary, otherwise handle the response
+        if let error = error {
+            displayAlertForError(error)
+        } else {
+            if let _ = response {
+                presentingViewController?.dismiss(animated: true, completion: nil)
+            }
+        }
+    }
+}
+
+// MARK: - Update UI
+extension ConfirmLocationViewController {
+    
+    /// Setup the UI to show that a network request is in progress.
+    private func prepareUIForNetworkRequest() {
+        finishButton.isEnabled = false
+        iconView.isHidden = true
+        indicatorView.startAnimating()
+    }
+    
+    /// Setup the UI to show that the network request is completed.
+    private func resetUIAfterNetworkRequest() {
+        finishButton.isEnabled = true
+        iconView.isHidden = false
+        indicatorView.stopAnimating()
     }
 }
